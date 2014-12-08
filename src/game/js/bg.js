10be0bb23fba3088aa1cde685f4e9c5e6d4af71a
Hunter Fitzwater <hcfitzwater@gmail.com>
2014-12-07 22:10:58 -0500

    simpler is better

--- src/game/js/bg.js
+++ src/game/js/bg.js
@@ -3,8 +3,7 @@ Bg = function( game ) {
 
 	game.physics.enable( [ this ], Phaser.Physics.ARCADE);
 
-	this.body.velocity.x = 0;
-	this.body.velocity.y = 0;
+	this.body.velocity.y = UTIL.random(5,8);
 
 	game.add.existing( this );
 };
@@ -14,29 +13,13 @@ Bg.prototype.constructor = Bg;
 
 Bg.prototype.update = function() {
 
-	this.body.velocity.y = UTIL.random(3,5);
 	if( this.y < -25 ) {
-		this.y = -25;
+		this.body.velocity.y *= -1;
+		this.y = -24;
 		
 	}	else if( this.y > 0 ) {
-		this.y = 0;
+		this.body.velocity.y *= -1;
+		this.y = -1;
 	}
-
-	this.body.velocity.x = UTIL.random(1,2);
-	if( this.x < -5 ) {
-		this.x = -5;
-	} else if( this.x > 5 ) {
-		this.x = 5;
-	}
-
-	this.rot = UTIL.random(5,8)/10;
-
-	if( this.body.angle > 5 ) {
-    	this.body.angle = 5;
-    	this.rot *= -1;
-    } else if( this.body.angle < -5 ) {
-    	this.body.angle = -5;
-    	this.rot *= -1;
-    }
 
 };
